0de4cfe22e33f6c137d84c9a3b9cc643e6760a29
meowtcha <hannahsheen12@gmail.com>
2026-06-19 02:18:43 +0800

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "@tailwindcss/vite"
-// import path from 'path'
-
-// export default defineConfig({
-//   assetsInclude: ['**/*.glb'],
-//   plugins: [react(),
-//             tailwindcss() 
-//             ],
-//   resolve: {
-//     	    alias: {
-//       		  "@": path.resolve(__dirname, "./src"),
-//     	    },
-//   	  }
-// })
-
-
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+  assetsInclude: ['**/*.glb'],
+  plugins: [react(),
+            tailwindcss() 
+            ],
+  resolve: {
+    	    alias: {
+      		  "@": path.resolve(__dirname, "./src"),
+    	    },
+  	  }
+})
+
+
+
